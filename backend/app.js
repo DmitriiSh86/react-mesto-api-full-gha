54de6eb0765e0bcbda3b8fcaa6514b8dd6cf9dda
Dmitrii Shatov <Dmitrii.Sh86@yandex.ru>
2023-08-08 17:19:44 +0300

    logger4

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 const { errors } = require('celebrate');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/error-handler');
 
 const app = express();
@@ -20,11 +19,7 @@ app.use(cors({ origin: 'https://dmitrii-mesto.nomoreparties.co', credentials: tr
 app.use(cookies());
 app.use(express.json());
 
-app.use(requestLogger);
-
 app.use(router);
-
-app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
